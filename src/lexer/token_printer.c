db981fdb38ba16fed4f54c53050d5f24d66386f7
Thomas Galateau <thomas.galateau@epita.fr>
2024-01-09 23:27:08 +0100

    feat: lexer

---
+++ src/lexer/token_printer.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include "lexer.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return 1;
+
+    struct lexer *lexer = lexer_new(argv[1]);
+    struct token token = lexer_pop(lexer);
+
+    while (token.type != TOKEN_EOF)
+    {
+        if (token.type == TOKEN_WORD)
+            printf("%s\n", token.value);
+        else if (token.type == TOKEN_ERROR)
+        {
+            printf("ERROR\n");
+            break;
+        }
+        else
+        {
+            printf("REAL ERROR\n");
+            break;
+        }
+
+        token = lexer_pop(lexer);
+    }
+
+    if (token.type == TOKEN_EOF)
+        printf("EOF\n");
+
+    lexer_free(lexer);
+
+    return 0;
+}
