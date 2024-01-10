ff1d48d808907bb865b71287f19c10ecedf44d27
Thomas Galateau <thomas.galateau@epita.fr>
2024-01-10 21:56:54 +0100

    feat: create ast, create parser

--- src/lexer/token.h
+++ src/lexer/token.h
@@ -6,10 +6,42 @@
 
 enum token_type
 {
+    TOKEN_WORD,
+    // TOKEN_ASSIGNEMENT_WORD,
+    // TOKEN_NAME,
+    TOKEN_NEWLINE,
+    // TOKEN_IO_NUMBER,
+    /**** Operators ****/
+    // TOKEN_AND_IF,
+    // TOKEN_OR_IF,
+    // TOKEN_DSEMI,
+    // TOKEN_DLESS,
+    // TOKEN_DGREAT,
+    // TOKEN_LESSAND,
+    // TOKEN_GREATAND,
+    // TOKEN_LESSGREAT,
+    // TOKEN_DLESSDASH,
+    // TOKEN_CLOBBER,
+    /**** Reserved words ****/
+    // TOKEN_IF,
+    // TOKEN_THEN,
+    // TOKEN_ELSE,
+    // TOKEN_ELIF,
+    // TOKEN_FI,
+    // TOKEN_DO,
+    // TOKEN_DONE,
+    // TOKEN_CASE,
+    // TOKEN_ESAC,
+    // TOKEN_WHILE,
+    // TOKEN_UNTIL,
+    // TOKEN_FOR,
+    /**** Reserved words recognized when reserved word is recognized ****/
+    // TOKEN_LBRACE,
+    // TOKEN_RBRACE,
+    // TOKEN_BANG,
+    // TOKEN_IN,
     TOKEN_EOF,
-    TOKEN_ERROR,
-    TOKEN_OPERATOR,
-    TOKEN_WORD
+    TOKEN_ERROR
 };
 
 struct token
---
+++ src/parser/tests/parser_test.c
@@ -0,0 +1,26 @@
+#include "../../lexer/lexer.h"
+#include "../parser.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return 1;
+
+    struct lexer *lexer = lexer_new(argv[1]);
+
+    struct ast *ast;
+    enum parser_status status = parse_input(&ast, lexer);
+    if (status != PARSER_OK)
+    {
+        lexer_free(lexer);
+        return 1;
+    }
+
+    // print_ast(ast);
+    // printf("\n");
+
+    // ast_free(ast);
+    lexer_free(lexer);
+
+    return 0;
+}
