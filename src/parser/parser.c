9937aeedc4fa50abc5df820e5871196d59592177
Thomas Galateau <thomas.galateau@epita.fr>
2024-01-11 00:03:01 +0100

    fix: parser return now asts, new ast structs

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,84 +2,119 @@
 
 #include "parser.h"
 
-enum parser_status parse_input(struct ast **res, struct lexer *lexer)
+struct ast_input *parse_input(struct lexer *lexer)
 {
-    if (parse_list(res /* TODO */, lexer) == PARSER_UNEXPECTED_TOKEN)
+    struct ast_input *ast = new_ast_input();
+
+    if (!struct ast_list *baby = parse_list(lexer))
     {
         // printf("parse_input: parse_list received unexpected token\n");
-        return PARSER_UNEXPECTED_TOKEN;
+        return NULL;
     }
+
+    ast->value = baby;
+
     if (lexer_peek(lexer).type == TOKEN_NEWLINE)
     {
         // printf("parse_input: lexing newline\n");
         lexer_pop(lexer);
-        return PARSER_OK;
+        return ast;
     }
     if (lexer_peek(lexer).type == TOKEN_EOF)
     {
         // printf("parse_input: lexing eof\n");
-        return PARSER_OK;
+        return ast;
     }
 
     // printf("parse_input: unexpected token\n");
-    return PARSER_UNEXPECTED_TOKEN;
+    return NULL;
 }
 
-enum parser_status parse_list(struct ast **res, struct lexer *lexer)
+struct ast_list *parse_list(struct lexer *lexer)
 {
     // printf("parse_list\n");
-    return parse_and_or(res /* TODO */, lexer);
+    struct ast_list *ast = new_ast_list();
+
+    if (struct ast_and_or *baby = parse_and_or(lexer))
+    {
+        ast->value = baby;
+        return ast;
+    }
+
+    return NULL;
 }
 
-enum parser_status parse_and_or(struct ast **res, struct lexer *lexer)
+struct ast_and_or *parse_and_or(struct lexer *lexer)
 {
     // printf("parse_and_or\n");
-    return parse_pipeline(res /* TODO */, lexer);
+    struct ast_and_or *ast = new_ast_and_or();
+
+    if (struct ast_pipeline *baby = parse_pipeline(lexer))
+    {
+        ast->value = baby;
+        return ast;
+    }
+
+    return NULL;
 }
 
-enum parser_status parse_pipeline(struct ast **res, struct lexer *lexer)
+struct ast_pipeline *parse_pipeline(struct lexer *lexer)
 {
     // printf("parse_pipeline\n");
-    return parse_command(res /* TODO */, lexer);
+    struct ast_pipeline *ast = new_ast_pipeline();
+
+    if (struct ast_command *baby = parse_command(lexer))
+    {
+        ast->value = baby;
+        return ast;
+    }
+
+    return NULL;
 }
 
-enum parser_status parse_command(struct ast **res, struct lexer *lexer)
+struct ast_command *parse_command(struct lexer *lexer)
 {
     // printf("parse_command\n");
-    return parse_simple_command(res /* TODO */, lexer);
+    struct ast_command *ast = new_ast_command();
+
+    if (struct ast_simple_command *baby = parse_simple_command(lexer))
+    {
+        ast->value = baby;
+        return ast;
+    }
+
+    return NULL;
 }
 
-enum parser_status parse_simple_command(struct ast **res, struct lexer *lexer)
+struct ast_simple_command *parse_simple_command(struct lexer *lexer)
 {
     if (lexer_peek(lexer).type == TOKEN_WORD)
     {
         // printf("parse_simple_command: lexing word\n");
         struct token tok = lexer_pop(lexer);
-        // TODO AST
+        struct ast_simple_command *ast = new_ast_simple_command(tok->value);
 
-        while (parse_element(res /* TODO */, lexer) == PARSER_OK)
+        while (char *element = parse_element(lexer))
         {
             // printf("parse_simple_command: parse_list received parser_ok\n");
-            //  TODO AST ?
+            //  TODO ADD ELEMENT TO AST
         }
 
-        return PARSER_OK;
+        return ast;
     }
 
     // printf("parse_simple_command: unexpected token\n");
-    return PARSER_UNEXPECTED_TOKEN;
+    return NULL;
 }
 
-enum parser_status parse_element(struct ast **res, struct lexer *lexer)
+char *parse_element(struct lexer *lexer)
 {
     if (lexer_peek(lexer).type == TOKEN_WORD)
     {
         // printf("parse_element: lexing word\n");
         struct token tok = lexer_pop(lexer);
-        // TODO AST
-
-        return PARSER_OK;
+        return tok->value;
     }
 
-    return PARSER_UNEXPECTED_TOKEN;
+    return NULL;
 }
