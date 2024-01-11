74cff199251dc994e17a6f8956ac6fc389a0474d
Elena Villepreux <elena.villepreux@epita.fr>
2024-01-11 11:03:47 +0100

    fix(lexer): testsuite lexer operational

--- src/lexer/tests/testsuite.sh
+++ src/lexer/tests/testsuite.sh
@@ -1,68 +1,33 @@
 #!/bin/sh
 
-function check
+# Function output file
+function_output_file="output.txt"
+
+# Expected output file
+expected_output_file="expected_output.txt"
+
+# Run your function and store the output in a file
+run_test()
 {
-  var="$*"
-  DIFF=$(diff -u <( ./lexer ${var} 2>/dev/null) <( find ${var} 2>/dev/null))
-  if [ "$DIFF" != "" ]; then
-    printf "\e[31m[ FAIL ]\e[39m  ";
-    printf "%b\n" "$var"
-    printf "%s\n" $DIFF;
-  else
-    printf "\e[32m[ OK ]\e[39m\t  "
-    printf "%b\n" "$var"
-  fi
+    # Store the actual output
+    ./lexer "$1" > "$function_output_file"
+
+    # Store the expected output
+    echo -en "$2" > "$expected_output_file"
+
+# Check if the output file matches the expected output file
+if diff -q "$function_output_file" "$expected_output_file" > /dev/null; then
+    echo -e "\e[32mOK"
+else
+    echo -e "\e[31mDIFFERENT:\e[0m $1"
+
+    # Display the difference
+    echo -e "\e[31mDifference:\e[0m"
+    diff -u "$function_output_file" "$expected_output_file"
+fi
 }
 
-check
-check "../"
-check "src" "include/"
-check "src"
-check "src" "!" "-name src"
-check "src" "-name" "src"
-check "-print" "-print"
-check "-type" "d" "-name" "test*"
-check "-type" "f" "-a" "-name" "memory*"
-#check -L
-check "-q"
-check "../" "-type" "b"
-check "../" "-type" "c"
-check "../" "-type" "d"
-check "../" "-type" "f"
-check "../" "-type" "l"
-check "../" "-type" "p"
-check "../" "-type" "s"
-check "-print" "-o" "-print"
-check "-print" "-a" "-print"
-check "-type" "d" "!" "-name" "include"
-check "-type" "f" "-name" "memory*" "-o" "-type" "d" "-print"
-check "src/include/" "-print" "-a" "-user" "elena.villepreux"
-check "src/" "-type" "f" "-name" "*.o"
-check "(" "-name" "mem*" "-o" "!" "-name" "eval*" ")"
-check "src" "-exec" "pwd" ";"
-check "tests" "-exec" "echo" "{}" ";"
-check "src/include/" "-name" "mem*" "-exec" "echo" "{}" ";"
-#check "src" "-execdir" "echo" "{}" ";"
-check "!" "-name" "tests"
-check "!" "(" "-name" "tests" "-o" "-name" "src" ")"
-check "(" "-name" "*.c" "-o" "-name" "memory*" ")"
-check "(" ")"
-check "src/include/" "-perm" "-644"
-check "src/include/" "-perm" "/644"
-check "src/include/" "-perm" "644"
-check "src/include/" "-perm" "238"
-check "src/include/" "-perm" "21"
-check "src/include/" "!" "-type" "d"
-check "(" "-type" "f" "-name" "memory*" ")" "-o" "(" "-name" "memory*" ")"
-check "-name" "me*" "-o"  "!" "(" "-type" "d" "-a" "-name" "include" ")" "-o" "-name" "memory.o"
-check "src/include/" "-newer" "src/include/memory.h"
-check "src/include/" "-newer" "src/include/memory.h" "!" "-name" "*.h"
-#check "tests" "-name" "test1" "-delete" "-delete"
-#check "tests" "-name" "direct1" "-delete"
+run_test "echo toto" "WORD echo\nWORD toto\nEOF\n"
+run_test "ls -a ; echo bar" "WORD ls\nWORD -a\nDELIMITER ;\nWORD echo\nWORD bar\nEOF\n"
 
-
-./lexer smt > test.out
-echo res > test.ref
-DIFF test.out test.ref
-
-
+rm -f "output.txt" "expected_output.txt"
