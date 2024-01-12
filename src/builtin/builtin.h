0bc10317ca5145fa27a957b5c9761c748653f993
Hugo Saint-Raymond <hugo.saint-raymond@epita.fr>
2024-01-12 01:41:55 +0100

    fix(builtin): finish builtins & tests for it

---
+++ src/builtin/builtin.h
@@ -0,0 +1,11 @@
+#ifndef BUILTIN_H
+#define BUILTIN_H
+
+#include <stdio.h>
+
+int echo(char **argv, FILE *sout);
+int my_true(void);
+int my_false(void);
+
+#endif /* BUILTIN_H */
+
---
+++ tests/builtin/test_builtin_echo.c
@@ -0,0 +1,105 @@
+#include <builtin.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+TestSuite(builtin_echo, .init = cr_redirect_stdout);
+
+Test(builtin_echo, simple_string) {
+    char *cmd[] = {"hello", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "hello\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, no_newline) {
+    char *cmd[] = {"-n", "world", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "world";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, interpret_escape_characters) {
+    char *cmd[] = {"-e", "line1\nline2\ttab", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "line1\nline2\ttab\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, no_newline_multiple_arguments) {
+    char *cmd[] = {"-n", "multiple", "arguments", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "multiple arguments";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, interpret_escape_multiple_arguments) {
+    char *cmd[] = {"-e", "multiple", "\\t", "words", "\\n", "escape", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "multiple \t words \n escape\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, no_interpret_escape) {
+    char *cmd[] = {"-E", "line1\\nline2\\ttab", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "line1\\nline2\\ttab\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, combined_options) {
+    char *cmd[] = {"-n", "-e", "-E", "combined", "options", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "combined options";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, backslashes_and_tabs) {
+    char *cmd[] = {"text with \\\\ backslashes and \\t tab", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "text with \\\\ backslashes and \\t tab\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, escape_special_characters) {
+    char *cmd[] = {"-e", "words with \\\\\\\\ backslashes, \\\\t tabs, and \\\\n newlines", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "words with \\\\ backslashes, \\t tabs, and \\n newlines\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
+
+Test(builtin_echo, mixed_options_and_special_characters) {
+    char *cmd[] = {"-e", "-n", "mixing \\\\ options with \\\\ backslashes \\\\n", NULL};
+    int actual = echo(cmd, stdout);
+    char *str_expected = "mixing \\ options with \\ backslashes \\n";
+    int expected = 0;
+    fflush(NULL);
+    cr_expect_stdout_eq_str(str_expected);
+    cr_expect_eq(actual, expected, "Expec: %d. Got: %d", expected, actual);
+}
