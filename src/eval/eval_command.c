7a92ddbc1899e3ca93e7793285a246bedda7f5c3
Nathan Sue <nathan.sue@epita.fr>
2024-01-13 00:26:08 +0100

    fix: iteration eval_list

--- src/eval/eval_command.c
+++ src/eval/eval_command.c
@@ -2,6 +2,10 @@
 
 int eval_command(struct ast_command *ast)
 {
+    if (ast->simple_command == NULL)
+    {
+        return 0;
+    }
     //TODO recognize simple_command / shell_command
     return eval_simple_command(ast->simple_command);
 }
--- src/execution/execution_builtin.c
+++ src/execution/execution_builtin.c
@@ -5,6 +5,10 @@ int execution_builtin(char **args, enum builtin builtin)
     if (builtin == ECHO)
         return echo(args, stdout);
     if (builtin == BOOL_FALSE)
+    {
+        //printf("Hey I exec false\n");
         return my_false();
+    }
+    //printf("Hey I exec true\n");
     return my_true();
 }  
