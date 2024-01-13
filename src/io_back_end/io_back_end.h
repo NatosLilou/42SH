8be422f91d1d804f9c8c597fd90c0e7a97e19334
Nathan Sue <nathan.sue@epita.fr>
2024-01-13 14:39:38 +0100

    fix: io_back_end finish

--- src/io_back_end/io_back_end.h
+++ src/io_back_end/io_back_end.h
@@ -1,6 +1,8 @@
 #ifndef IO_BACK_END_H
 #define IO_BACK_END_H
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <fcntl.h>
 #include <fnmatch.h>
 #include <unistd.h>
@@ -11,7 +13,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
-char *io_back_end(int argc, char *argv[]);
+FILE *io_back_end_init(int argc, char *argv[]);
+char io_back_end_read(FILE *stream, long offset);
+void io_back_end_close(FILE *stream);
+//char *io_back_end(int argc, char *argv[]);
 
 #endif /* !IO_BACK_END_H */
