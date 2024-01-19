5da35be64a86517d1c01d312adf872801b61bd44
Nathan Sue <nathan.sue@epita.fr>
2024-01-19 21:50:38 +0100

    feat: 1st version of redir

--- src/eval_exec/eval_simple_command.c
+++ src/eval_exec/eval_simple_command.c
@@ -2,6 +2,17 @@
 
 int eval_simple_command(struct ast_simple_command *ast)
 {
+    int res_redir = 0;
+    size_t i = 0;
+    while (i < ast->pos_redir)
+    {
+        res_redir = eval_redir(ast->redir[i]);
+        if (res_redir != 0)
+        {
+            return 1;
+        }
+        i++;
+    }
     if (ast->commands)
     {
         return execution_simple_command(ast->commands); // C'est nul
--- src/eval_exec/execution_pipeline.c
+++ src/eval_exec/execution_pipeline.c
@@ -43,41 +43,7 @@ int execution_pipeline(struct ast_pipeline *ast)
     int fds[2];
     if (pipe(fds) == -1)
     {
-        pipe(fds_temp);
-        pid_t pid = fork();
-        if (pid != 0)
-        {
-            if (i != 0)
-            {
-                close(fds[0]);
-                close(fds[1]);
-            }
-            i++;
-            fds[0] = fds_temp[0];
-            fds[1] = fds_temp[1];
-            int wstatus;
-            waitpid(pid,&wstatus,0);
-            res = WEXITSTATUS(wstatus);
-        }
-        else
-        {
-            if (i != 2)
-                dup2(fds_temp[1], STDOUT_FILENO);
-            if (i != 0)
-            {
-                dup2(fds[0], STDIN_FILENO);
-                close(fds[1]);
-            }
-            close(fds[0]);
-            close(fds_temp[0]);
-            close(fds_temp[1]);
-            int res = eval_command(ast->commands[i]);
-            if (res == 0)
-            {
-                exit(EXIT_SUCCESS);
-            }
-            errx(res, "execvp failed");
-        }
+        errx(1, "pipe faied");
     }
     int res = 0;
     size_t i = 0;
