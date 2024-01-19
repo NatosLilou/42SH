22eade92006e90baa26e5fded0aaf78e191abb80
Nathan Sue <nathan.sue@epita.fr>
2024-01-19 11:39:14 +0100

    feat: temp for pipe

--- src/eval_exec/execution_pipeline.c
+++ src/eval_exec/execution_pipeline.c
@@ -1,51 +1,69 @@
 #include "execution.h"
 
-
-static pid_t exec_fork(struct ast_command *ast, int fds[2], int side)
+void execute(struct ast_command *ast, int input_fd, int ouput_fd)
 {
     pid_t pid = fork();
-    if (pid != 0)
-        return pid;
 
-    int fd_to = side == 0 ? STDOUT_FILENO : STDIN_FILENO;
-    int fd_final = side == 0 ? fds[1] : fds[0];
-
-    if (dup2(fd_final, fd_to) == -1)
+    if (pid == -1)
     {
-        errx(1, "dup2 failed");
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
-    int res = eval_command(ast);
-    close(fds[0]);
-    close(fds[1]);
-    if (res == 1)
-        errx(1, "eval_failed %d failed", side);
-   exit(EXIT_SUCCESS);
+
+    if (pid == 0)
+    {
+        if (input_fd != STDIN_FILENO)
+        {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        }
+
+        if (ouput_fd != STDOUT_FILENO)
+        {
+            dup2(ouput_fd, STDOUT_FILENO);
+            close(ouput_fd);
+        }
+
+        execvp(ast->simple_command->commands[0], ast->simple_command->commands);
+
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
 }
+
 
 int execution_pipeline(struct ast_pipeline *ast)
 {
+    int input_fd = STDIN_FILENO;
     int fds[2];
     if (pipe(fds) == -1)
     {
         errx(1, "pipe failed");
     }
-    int wstatus;
-    int pid_first = exec_fork(ast->commands[0], fds, 0);
-    waitpid(pid_first, &wstatus, 0);
-    int res = WEXITSTATUS(wstatus);
-    printf("the first exit = %d \n", res);
-    printf("ast->pos = %zu\n", ast->pos);
-    size_t i = 1;
+    size_t i = 0;
     while (i < ast->pos)
     {
-        printf("hey i try the %zu enieme command\n", i);
-        int pid_right = exec_fork(ast->commands[i], fds, 1);
-        waitpid(pid_right, &wstatus, 0);
-        res = WEXITSTATUS(wstatus);
+        if (i + 1 < ast->pos && pipe(fds) == -1)
+        {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+
+        execute(ast->commands[i], input_fd, (i + 1 < ast->pos) ? fds[1] : STDOUT_FILENO);
+
+        if (input_fd != STDIN_FILENO)
+        {
+            close(input_fd);
+        }
+
+        if (i + 1 < ast->pos)
+        {
+            close(fds[1]);
+            input_fd = fds[0];
+        }
         i++;
     }
+    while (wait(NULL) > 0);
 
-    close(fds[0]);
-    close(fds[1]);
-    return res;
+    return 0;
 }
