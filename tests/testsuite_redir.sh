c1fef07a04d5403dc9d712a317bcc633f40d38a7
Nathan Sue <nathan.sue@epita.fr>
2024-01-24 17:37:13 +0100

    feat: add test for redir

--- tests/testsuite_redir.sh
+++ tests/testsuite_redir.sh
@@ -133,6 +133,9 @@ run_test_redir()
     # Store the actual output and stderr
     ./src/./42sh < "$script" > "$my_file_out" 2> "$my_file_err"
     echo $? > "$my_exit_code"
+
+    cat "$2" > save
+    rm "$2"
     # Store the expected output and stderr
     bash --posix < "$script" > "$ref_file_out" 2> "$ref_file_err"
     echo $? > "$ref_exit_code"
@@ -143,7 +146,7 @@ run_test_redir()
             ([ -s "$my_file_err" ] && [ -s "$ref_file_err" ]) || 
             ([ ! -s "$my_file_err" ] && [ ! -s "$ref_file_err" ])
         ) &&      
-        #diff -q "$my_file_err" "$ref_file_err" > /dev/null &&
+        diff -q "$2" save > /dev/null &&
         diff -q "$my_exit_code" "$ref_exit_code" > /dev/null; then
 
         #echo -ne "$BLUE Test ${CMPT}... $WHITE"
@@ -162,26 +165,54 @@ run_test_redir()
 }
 
 # ============================== Test STRING =================================
-run_test_string "> uwu echo toto je"
-run_test_string "echo tata > test jambon > test2"
-run_test_string "echo tonton >> test"
-
+run_test_string "> uwu echo toto je" "uwu"
+run_test_string "ls >| test jambon >| test2" "test2"
+run_test_string "ls > test jambon > test2" "test2"
+run_test_string "ls .. > test ;echo tonton >> test" "test"
+run_test_string "ls toto > test; cat test" "test"
+run_test_string "ls < test; cat test" "test"
+run_test_string "1> ls | cat echo" "echo"
+run_test_string "ls < test < test1" "test1"
+run_test_string "ls 0> uwu" "uwu"
+run_test_string "ls 1> hey" "hey"
+run_test_string "uwu 2< sake" "sake"
 # ============================== Test FILE ===================================
-run_test_file "> uwu echo toto je"
-run_test_file "echo tata > test jambon > test2"
-run_test_file "echo tonton >> test"
-
+run_test_file "> uwu echo toto je" "uwu"
+run_test_file "ls >| test jambon >| test2" "test2"
+run_test_file "ls > test jambon > test2" "test2"
+run_test_file "ls .. > test ;echo tonton >> test" "test"
+run_test_file "ls toto > test; cat test" "test"
+run_test_file "ls < test; cat test" "test"
+run_test_file "1> ls | cat echo" "echo"
+run_test_file "ls < test < test1" "test1"
+run_test_file "ls 0> uwu" "uwu"
+run_test_file "ls 1> hey" "hey"
+run_test_file "uwu 2< sake" "sake"
 # ============================== Test PIPE ===================================
-run_test_pipe "> uwu echo toto je"
-run_test_pipe "echo tata > test jambon > test2"
-run_test_pipe "echo tonton >> test"
-
+run_test_pipe "> uwu echo toto je" "uwu"
+run_test_pipe "ls >| test jambon >| test2" "test2"
+run_test_pipe "ls > test jambon > test2" "test2"
+run_test_pipe "ls .. > test ;echo tonton >> test" "test"
+run_test_pipe "ls toto > test; cat test" "test"
+run_test_pipe "ls < test; cat test" "test"
+run_test_pipe "1> ls | cat echo" "echo"
+run_test_pipe "ls < test < test1" "test1"
+run_test_pipe "ls 0> uwu" "uwu"
+run_test_pipe "ls 1> hey" "hey"
+run_test_pipe "uwu 2< sake" "sake"
 # ============================== Test REDIR ==================================
-run_test_redir "> uwu echo toto je"
-run_test_redir "echo tata > test jambon > test2"
-run_test_redir "echo tonton >> test"
-
+run_test_redir "> uwu echo toto je" "uwu"
+run_test_redir "ls >| test jambon >| test2" "test2"
+run_test_redir "ls > test jambon > test2" "test2"
+run_test_redir "ls .. > test ;echo tonton >> test" "test"
+run_test_redir "ls toto > test; cat test" "test"
+run_test_redir "ls < test; cat test" "test"
+run_test_redir "1> ls | cat echo" "echo"
+run_test_redir "ls < test < test1" "test1"
+run_test_redir "ls 0> uwu" "uwu"
+run_test_redir "ls 1> hey" "hey"
+run_test_redir "uwu 2< sake" "sake"
 # ============================== THE END =====================================
-rm -f $ref_file_out $my_file_out $ref_file_err $my_file_err $script uwu test2
+rm -f $ref_file_out $my_file_out $ref_file_err $my_file_err $script uwu test2 test1 hey test sake echo
 
 echo -e "$GREEN Tests passed ${CMPT_SUCCEED} $BLUE|$RED Tests failed ${CMPT_FAILED} $BLUE|$YELLOW $((CMPT_SUCCEED*100/CMPT)) %$WHITE"
