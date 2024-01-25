8e82322ebe9876be4ce4582587a4815446ca3b94
elena.villepreux <elena.villepreux@epita.fr>
2024-01-25 20:43:11 +0100

    create: general shell script for test

---
+++ tests/test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#Execute the scripts which have in their name:
+# - the category
+# - the sort of input
+# - both
+execute_selected_tests()
+{
+    first="$1"
+    second="$2"
+
+    for script in tests/*.sh; do
+        if [ -x "$script" ] && [ -n "$(echo "$script" | grep "$first")" ] && [ -n "$(echo "$script" | grep "$second")" ]; then
+                echo "Execution de $script"
+                ./"$script"
+        fi
+    done
+}
+
+execute_all_tests()
+{
+    for script in *.sh; do
+        if [ -x "$script" ]; then
+            tests/./"$script"
+        fi
+    done
+}
+
+if [ "$#" -eq 0 ]; then
+    execute_all_tests
+
+elif [ "$#" -eq 1 ]; then
+    execute_selected_tests "$1"
+
+elif [ "$#" -eq 2 ]; then
+    execute_selected_tests "$1" "$2"
+
+else
+    exit 1
+fi
