0df4445d8856e0f5da8d857aeb08f588a9a18928
Thomas Galateau <thomas.galateau@epita.fr>
2024-01-27 19:47:29 +0100

    fix: add tests

--- tests/others/testsuite_coverage.sh
+++ tests/others/testsuite_coverage.sh
@@ -12,6 +12,8 @@ run_test()
 run_test "echo $$"
 run_test "echo $@"
 run_test "echo $RANDOM"
+run_test "export tata=uwu"
+run_test "export tata"
 run_test "tata=toto; echo \$tata; tata() { if (echo \$tata); then echo \$tata; fi }; tata"
 run_test "tat=tot; tat=uwu; while false; do echo \$tat; done; until true; do echo \$tat; done"
 
